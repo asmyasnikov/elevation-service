f404c35898029b3136fb4ac76e3d6d0c16da8695
Aleksey Myasnikov <79263256394@ya.ru>
2020-05-14 17:00:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {json, send} = require('micro')
-const addElevation = require('geojson-elevation').addElevation
+const coordEach = require('@turf/meta').coordEach;
 const TileSet = require('node-hgt').TileSet
 const ImagicoElevationDownloader = require('node-hgt').ImagicoElevationDownloader
 const tileDirectory = process.env.TILE_DIRECTORY || './data'
@@ -9,6 +9,39 @@ const tileDownloader = process.env.TILE_DOWNLOADER === 'none'
   : new ImagicoElevationDownloader(tileDirectory)
 const maxPostSize = process.env.MAX_POST_SIZE || "500kb";
 const tiles = new TileSet(tileDirectory, {downloader:tileDownloader});
+
+const addElevation = function(geojson, elevationProvider, cb, nodata) {
+  var waiting = 0,
+      allProcessed = false;
+
+  coordEach(geojson, function(coords) {
+      waiting++;
+
+      elevationProvider.getElevation([coords[1], coords[0]], function(err, elevation) {
+          if (err) {
+              if (nodata != null) {
+                  coords[2] = nodata;
+                  waiting--;
+              } else {
+                  cb(err);
+                  waiting = -1;
+              }
+          } else {
+              coords[2] = elevation;
+              waiting--;
+          }
+
+          if (allProcessed && waiting === 0) {
+              cb(undefined, geojson);
+          }
+      });
+  });
+
+  allProcessed = true;
+  if (waiting === 0) {
+      cb(undefined, geojson);
+  }
+}
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
